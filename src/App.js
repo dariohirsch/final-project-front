dde748d967fe7246db7fcb1005ca19566599551e
dariohirsch <darohirsch@gmail.com>
2021-09-29 09:32:03 +0200

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import "./App.css";
-import { Switch, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage";
-import ProjectListPage from "./pages/ProjectListPage";
-import ProjectDetailsPage from "./pages/ProjectDetailsPage";
-import EditProjectPage from "./pages/EditProjectPage";
+import "./App.css"
+import { Switch, Route } from "react-router-dom"
+import Navigation from "./components/Navbar"
+import HomePage from "./pages/HomePage"
+import ProjectListPage from "./pages/ProjectListPage"
+import ProjectDetailsPage from "./pages/ProjectDetailsPage"
+import EditProjectPage from "./pages/EditProjectPage"
 
-import SignupPage from "./pages/SignupPage";
-import LoginPage from "./pages/LoginPage";
-import PrivateRoute from "./components/PrivateRoute";    // <== IMPORT
-import AnonRoute from "./components/AnonRoute";        // <== IMPORT
-
+import SignupPage from "./pages/SignupPage"
+import LoginPage from "./pages/LoginPage"
+import PrivateRoute from "./components/PrivateRoute" // <== IMPORT
+import AnonRoute from "./components/AnonRoute" // <== IMPORT
+import { Container } from "react-bootstrap"
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
+	return (
+		<div className="App">
+			<Navigation />
+			<Container>
+				<Switch>
+					<Route exact path="/" component={HomePage} />
 
-      <Switch>      
-        <Route exact path="/" component={HomePage} />
-        
-        {/* 👇 UPDATE THE EXISTING ROUTES 👇  */}
-        <PrivateRoute exact path="/projects" component={ProjectListPage} />
-        <PrivateRoute exact path="/projects/:id" component={ProjectDetailsPage} />
-        <PrivateRoute exact path="/projects/edit/:id" component={EditProjectPage} />
-        
-        <AnonRoute exact path="/signup" component={SignupPage} />
-        <AnonRoute exact path="/login" component={LoginPage} />
-      </Switch>
-    </div>
-  );
+					{/* 👇 UPDATE THE EXISTING ROUTES 👇  */}
+					<PrivateRoute exact path="/projects" component={ProjectListPage} />
+					<PrivateRoute exact path="/projects/:id" component={ProjectDetailsPage} />
+					<PrivateRoute exact path="/projects/edit/:id" component={EditProjectPage} />
+
+					<AnonRoute exact path="/signup" component={SignupPage} />
+					<AnonRoute exact path="/login" component={LoginPage} />
+				</Switch>
+			</Container>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,43 @@
-import { Link } from "react-router-dom";
-import { useContext } from "react";                       // <== IMPORT 
-import { AuthContext } from "./../context/auth.context";  // <== IMPORT
+import { useContext } from "react" // <== IMPORT
+import { AuthContext } from "./../context/auth.context" // <== IMPORT
+import { Container, Nav, Navbar, NavDropdown, Button } from "react-bootstrap"
 
-function Navbar() {
-  // Subscribe to the AuthContext to gain access to
-  // the values from AuthContext.Provider `value` prop
-  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+function Navigation() {
+	// Subscribe to the AuthContext to gain access to
+	// the values from AuthContext.Provider `value` prop
+	const { isLoggedIn, user, logOutUser } = useContext(AuthContext)
 
-  return (
-    <nav>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
--
-      {isLoggedIn
-        ? (<>
-            <Link to="/projects">
-              <button>Projects</button>
-            </Link>
-            <button onClick={logOutUser}>Logout</button>
-            <span>{user.name}</span>
-          </>)
-        : 
-        (<>
-          <Link to="/signup"> <button>Signup</button> </Link>
-          <Link to="/login"> <button>Login</button> </Link>
-        </>)
-      }
-    </nav>
-  );
+	return (
+		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+			<Container>
+				<Navbar.Brand href="/">BetFriends</Navbar.Brand>
+				<Nav className="me-auto">
+					<Nav.Link href="#pricing">Upcoming matches</Nav.Link>
+					<NavDropdown title="Leagues" id="collasible-nav-dropdown">
+						<NavDropdown.Item href="#action/3.1">View all leagues</NavDropdown.Item>
+						<NavDropdown.Item href="#action/3.2">My leagues</NavDropdown.Item>
+						<NavDropdown.Item href="#action/3.3">Create new league</NavDropdown.Item>
+					</NavDropdown>
+				</Nav>
+				{isLoggedIn ? (
+					<>
+						<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+						<Navbar.Collapse id="responsive-navbar-nav">
+							<Button onClick={logOutUser}>Logout</Button>
+							<span>{user.name}</span>
+						</Navbar.Collapse>
+					</>
+				) : (
+					<>
+						<Nav>
+							<Nav.Link href="/login">Log In</Nav.Link>
+							<Nav.Link href="/signup">Sign Up</Nav.Link>
+						</Nav>
+					</>
+				)}
+			</Container>
+		</Navbar>
+	)
 }
 
-export default Navbar;
+export default Navigation
